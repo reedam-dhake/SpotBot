35fe7e03d34d451d57cdf2069737ac80d1a34a4b
unknown <vedantdeo1@gmail.com>
2021-03-06 12:39:24 +0530

    edited Login.py

--- Login.py
+++ Login.py
@@ -12,7 +12,14 @@ print("2. Apple ID")
 print("3. Google")
 print("4. Phone number")
 print("5. Username and Password\n")
-loginMethod = int(input())
+loginMethod=0
+while(True):
+    loginMethod = input().strip()
+    if loginMethod.isnumeric():
+        loginMethod=int(loginMethod)
+        break
+    else:
+        print("Not a valid entry.")
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 #options = webdriver.ChromeOptions()
 #options.headless = True
@@ -43,6 +50,26 @@ elif loginMethod == 2:
 elif loginMethod == 3:
     google = loginLinks[2]
     google.click()
+
 elif loginMethod == 4:
     phoneNumber = loginLinks[3]
     phoneNumber.click()
+    phonenum=input("Enter your 10 digit phone number: ").strip()
+    p=driver.find_element_by_id("phonelogin-phonenumber")
+    p.send_keys(phonenum)
+    enterbutton=driver.find_element_by_id("phonelogin-button")
+    enterbutton.click()
+    otp=input("Enter the 6 digit OTP sent to your phone number: ").strip()
+    q=driver.find_element_by_id("phonelogin-code")
+    q.send_keys(otp)
+    button=driver.find_element_by_id("code-button")
+    button.click()
+elif loginMethod == 5:
+    directlogin = loginLinks[4]
+    directlogin.click()
+    Username = input("Enter Username: ").strip()
+    Password = input("Enter Password: ").strip()
+    username = driver.find_element_by_id("login-username")
+    username.send_keys(Username)
+    passw = driver.find_element_by_id("login-password")
+    passw.send_keys(Password)
